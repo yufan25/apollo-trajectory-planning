537c25dba76fb41a30a07a3e33aac497904a3948
yufan25 <98960981+yufan25@users.noreply.github.com>
2022-02-25 22:34:21 +0800

    Add files via upload

---
+++ Apollo/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/yufan/Desktop/ros/Apollo/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/yufan/Desktop/ros/learn/devel;/opt/ros/melodic'.split(';') if '/home/yufan/Desktop/ros/learn/devel;/opt/ros/melodic' != '' else []
