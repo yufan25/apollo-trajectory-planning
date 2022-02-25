483afaef1931dafe8181d60cc5b9960c115f216c
yufan25 <98960981+yufan25@users.noreply.github.com>
2022-02-25 22:31:27 +0800

    Add files via upload

---
+++ Apollo/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/yufan/Desktop/ros/Apollo/src
+# Build directory: /home/yufan/Desktop/ros/Apollo/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("apollo_planning")
---
+++ Apollo/build/gtest/googlemock/gtest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /usr/src/googletest/googletest
+# Build directory: /home/yufan/Desktop/ros/Apollo/build/gtest/googlemock/gtest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
